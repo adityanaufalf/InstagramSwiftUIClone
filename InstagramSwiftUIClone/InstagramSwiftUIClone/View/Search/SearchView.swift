0cde476ec4646731ef38996b27afebbed72b3f35
Aditya Farhan <adit@happy5.co>
2021-04-11 15:54:50 +0700

    Create search view section in home page

--- InstagramSwiftUIClone/InstagramSwiftUIClone/View/Search/SearchView.swift
+++ InstagramSwiftUIClone/InstagramSwiftUIClone/View/Search/SearchView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State private var text: String = ""
+    @State private var isEditing: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            SearchBar(text: $text, isEditing: $isEditing)
+                .padding()
+            
+            if isEditing {
+                UserListView()
+            } else {
+                PostGridView()
+            }
+        }
     }
 }
 
