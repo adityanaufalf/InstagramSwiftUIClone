a25e5924b37461274842210f827b89e46455c659
Aditya Farhan <adit@happy5.co>
2021-04-17 09:44:29 +0700

    Create section upload post view in home page

--- InstagramSwiftUIClone/InstagramSwiftUIClone/View/Upload Post/UploadPostView.swift
+++ InstagramSwiftUIClone/InstagramSwiftUIClone/View/Upload Post/UploadPostView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    
+    @State private var selectedImage: UIImage?
+    @State private var image: Image?
+    @State private var caption: String = ""
+    @State private var isShowPicker = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 24) {
+            if let image = image {
+                HStack(alignment: .top, spacing: 16) {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 96, height: 96, alignment: .center)
+                    
+                    TextField("Enter your caption...", text: $caption)
+                        .font(.system(size: 14))
+                    
+                }
+                Button(action: {
+                }, label: {
+                    Text("Share")
+                        .foregroundColor(.white)
+                        .font(.system(size: 14, weight: .semibold, design: .default))
+                })
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding(.vertical, 10)
+                .background(Color.blue)
+                .cornerRadius(4)
+                Spacer()
+            } else {
+                Button(action: {
+                    isShowPicker.toggle()
+                }, label: {
+                    Image("ico-add-photo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 96, height: 96, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }).sheet(isPresented: $isShowPicker, onDismiss: loadImage) {
+                    ImagePicker(selectedImage: $selectedImage)
+                }.padding(.top, 64)
+            }
+        }.padding()
+    }
+}
+
+extension UploadPostView {
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        image = Image(uiImage: selectedImage)
     }
 }
 
