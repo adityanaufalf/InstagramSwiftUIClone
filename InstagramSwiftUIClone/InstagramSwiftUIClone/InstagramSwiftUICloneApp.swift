7981f5617ec2a2f52ba4d7f3cb8642466485fa58
Aditya Farhan <adit@happy5.co>
2021-04-17 10:53:43 +0700

    Pod file init and init firebase depedencies

--- InstagramSwiftUIClone/InstagramSwiftUIClone/InstagramSwiftUICloneApp.swift
+++ InstagramSwiftUIClone/InstagramSwiftUIClone/InstagramSwiftUICloneApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct InstagramSwiftUICloneApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
