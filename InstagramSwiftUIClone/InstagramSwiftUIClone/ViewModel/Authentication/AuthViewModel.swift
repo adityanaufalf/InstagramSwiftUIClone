2d47d009aa5b600af70a27fbdb529af05c41747c
Aditya Farhan <adit@happy5.co>
2021-04-17 15:39:37 +0700

    Create AuthViewModel that connected to Firebase for Sign In, Sign Up and Logout(Revision 1)

--- InstagramSwiftUIClone/InstagramSwiftUIClone/ViewModel/Authentication/AuthViewModel.swift
+++ InstagramSwiftUIClone/InstagramSwiftUIClone/ViewModel/Authentication/AuthViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import SwiftUI
+import Firebase
+
+class AuthViewModel: ObservableObject {
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        userSession = Auth.auth().currentUser
+    }
+    
+    func doLogin(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to signed in : \(error.localizedDescription)")
+            } else {
+                guard let user = result?.user else { return }
+                self.userSession = user
+            }
+        }
+    }
+    
+    func doSignUp(withEmail email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to signed up : \(error.localizedDescription)")
+            } else {
+                guard let user = result?.user else { return }
+                self.userSession = user
+            }
+        }
+    }
+    
+    func signOut() {
+        self.userSession = nil
+        try? Auth.auth().signOut()
+    }
+    
+    func resetPassword(){
+        print("This is reset")
+
+    }
+    
+    func fetchUser() {
+        print("This is fetch")
+
+    }
+}
